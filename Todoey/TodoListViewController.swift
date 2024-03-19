9237e8c1df49e23f408f1938c30b12e2325801cd
hfyZone <1178805691@qq.com>
2024-03-19 22:55:05 +0800

    在CV层用硬编码实现备忘录增加功能

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class TodoListViewController: UITableViewController {
 
     //拟定三个备忘字符串
-    let itemArray = ["起床", "做早饭吃早饭", "工作"]
+    var itemArray = ["起床", "做早饭吃早饭", "工作"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,10 +40,35 @@ class TodoListViewController: UITableViewController {
         //指定tv当前的cell的标记符号为选中/取消选中
         let currentCell = tableView.cellForRow(at: indexPath)
         currentCell?.accessoryType = currentCell?.accessoryType == .checkmark ? .none : .checkmark
-        
-        
-        
     }
+    
+    //MARK: - 新增项目
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        //新建一个alert窗口，用来给用户新建备忘录
+        let alert = UIAlertController(title: "新建备忘录", message: "", preferredStyle: .alert)
+        //新建一个alert的action（按钮），在回调函数中确定用户点击”增加“按钮的时候的行动
+        let action = UIAlertAction(title: "增加", style: .default){
+            (action) in
+            //将新增备忘录加入备忘录列表中
+            self.itemArray.append(textField.text!)
+            //触发tv的数据渲染流程
+            self.tableView.reloadData()
+            
+        }
+        //为alert装载一个输入框,并将闭包内输入框状态传至方法内的输入框（引用传递)
+        //此方法是在alert的输入框初始化的时候运行，因此用户输入的数据无法获取
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "请填写备忘内容"
+            textField = alertTextField
+        }
+        //为alert装载action
+        alert.addAction(action)
+        //触发alert
+        present(alert, animated: true, completion: nil)
+    }
+    
     
 
  
