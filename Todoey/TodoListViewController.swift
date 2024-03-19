e1906d006b3c73d941019857f7e09ac4db0a1698
hfyZone <1178805691@qq.com>
2024-03-19 21:31:43 +0800

    新建UITableViewController，并实现对其的硬编码数据渲染

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class TodoListViewController: UITableViewController {
 
+    //拟定三个备忘字符串
+    let itemArray = ["起床", "做早饭吃早饭", "工作"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    //MARK: - TableView的数据源方法
+    //给出一个tableView有多少条数据
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    //根据IndexPath生成cell并返回给委托
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]//TODO: cell.textLabel已标记弃用，学习UIListContentConfiguration
+        return cell
+    }
+    
 
-
+ 
 }
 
